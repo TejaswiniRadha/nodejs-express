af2ccd11e4ed10d80413e494bd7dedb525bae41f
TejaswiniRadha <teju.nama@gmail.com>
2021-07-23 11:02:42 +0000

    "first commit"

--- createserver/app.js
+++ createserver/app.js
@@ -1,3 +1,4 @@
-const http= require('http');
-const Server=http.createServer((req,res)=>{res.end("tejaswininama")})
-Server.listen(3000,()=>{});
+/* const http= require('http');
+const Server=http.createServer((req,res)=>{res.end("Tejaswininamaradhaejaswininama")})
+Server.listen(4000,()=>{}); */
+const http=require('http');
